cbce4c5a12a8e5277051f3b9913ec8ef5f9cc7a1
maysara9900 <maysaradeveloper@gmail.com>
2022-08-15 16:50:12 +0100

    initial commit

---
+++ login.c
@@ -0,0 +1,38 @@
+/*
+    IMPORTANT: This file has been compiled using this flag: -Xclang -cheri-bounds=subobject-safe
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ID_LENGTH 8
+
+struct user {
+  char user_id[ID_LENGTH];
+  // should be 0 for non admin users
+  int admin;
+} u;
+
+char get_random_char(){
+  return 'A' + (random() % 26);
+}
+
+void
+create_non_admin_user()
+{
+  u.admin = 0;
+  for(size_t i = 0; i <= ID_LENGTH; i++){
+    u.user_id[i] = get_random_char();
+  }
+}
+
+int
+main(void)
+{
+  create_non_admin_user();
+  if(u.admin != 0){
+      printf("user should not be admin!");
+      exit(1);
+  }
+  return 0;
+}
+
